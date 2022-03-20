a1cd3a0d57fe81346ced9d00fdd04b3ea05e2331
Ruchi <rc382783@dal.ca>
2022-03-19 23:58:55 -0300

    modified api structure

--- ecofresh/server/config.js
+++ ecofresh/server/config.js
@@ -2,6 +2,6 @@
  * @author Ruchi Shinde
  */
 
-const ATLAS_URI="mongodb+srv://kandarp:Kandarp%40123@tutorial5-cluster.orkpa.mongodb.net/Ecofresh?retryWrites=true&w=majority"
+const ATLAS_URI="mongodb+srv://csci5709_g10:Winter%402022@cluster0.uvfop.mongodb.net/ecofresh?retryWrites=true&w=majority"
 
 module.exports = ATLAS_URI
