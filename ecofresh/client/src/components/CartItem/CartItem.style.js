4415cc964e427d68b7aa586126ca90cec1719caf
Mugdha Anil Agharkar <mg425404@dal.ca>
2022-04-01 01:11:09 -0300

    WIP: Add code to render and delete items from cart

---
+++ ecofresh/client/src/components/CartItem/CartItem.style.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const CartItemContainer = styled.div`
+    display: flex;
+`;
+
+const ImageContainer = styled.div``;
+
+const ContentContainer = styled.div``;
+
+const DeleteButtonContainer = styled.div``;
+
+const RecipeName = styled.h3``;
+
+const RecipeContent = styled.div``;
+
+
+export { CartItemContainer, ImageContainer, ContentContainer, DeleteButtonContainer,
+    RecipeName, RecipeContent };
